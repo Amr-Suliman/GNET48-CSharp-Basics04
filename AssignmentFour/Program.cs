90f8bd569848e13ecd6ef51e3b0ad19f1570cc90
Amr Suliman <amrelgohary573@gmail.com>
2026-09-20 05:15:08 +0300

    Question 01

--- AssignmentFour/Program.cs
+++ AssignmentFour/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+
+            #region Question 01
+
+            //double[] prices = { 25.5, 40.0, 33.75 };
+            //Console.WriteLine($"index: 1, value: {prices[1]}");
+
+            #endregion
+
+
         }
     }
 }
