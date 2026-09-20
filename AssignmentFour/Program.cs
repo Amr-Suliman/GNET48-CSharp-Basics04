27c95f28791df9a40b720cfa3ac4aefbd04a33cc
Amr Suliman <amrelgohary573@gmail.com>
2026-09-20 05:36:43 +0300

    Question 02

--- AssignmentFour/Program.cs
+++ AssignmentFour/Program.cs
@@ -14,6 +14,19 @@
 
             #endregion
 
+            #region Question 02
+
+            //int[,] shlefCopies = new int[2, 2]
+            //{
+            //    {3,5 },
+            //    {1,4 }
+            //};
+            //Console.WriteLine(shlefCopies.GetLength(0));
+            //Console.WriteLine(shlefCopies[1,0]); // result 1
+
+            #endregion
+
+
 
         }
     }
